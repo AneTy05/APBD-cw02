582b89efee82a64187966b721e6b49ff353abd86
Aneta Tyrna <s28508@pjwstk.edu.pl>
2025-03-27 20:18:37 +0100

    Container ready

--- cw02/cw02/Container.cs
+++ cw02/cw02/Container.cs
@@ -2,12 +2,41 @@
 
 public class Container
 {
-    public double MaxLoad { get; set; } //in kg
-    private double _masaLadunku; //w kilogramach
-    private double _wysokosc; //w centymertach
-    private double _wagaWlasna; //waga samego kontenera, w kilogramach
-    private double _glebokosc; //w centymetrach
-    private string _nrSeryjny1 = "CON";
-    private string? _nrSeryjny2;
-    private static int counter = 0; 
+    public double MaxLoad { get; } //in kg
+    public double CurrentLoad { get; set; } //in kg
+    public double Height { get; } //in cm
+    public double OwnWeight { get; } //waga samego kontenera, in kg
+    public double Depth { get; } //in cm
+    public string SerialNumber { get; }
+    private static int counter = 1; 
+    
+    protected Container(string type, double maxLoad, double ownWeight, double height, double depth)
+    {
+        SerialNumber = $"KON-{type}-{counter++}";
+        MaxLoad = maxLoad;
+        OwnWeight = ownWeight;
+        Height = height;
+        Depth = depth;
+        CurrentLoad = 0;
+    }
+    
+    public void Load(double weight)
+    {
+        if (CurrentLoad + weight > MaxLoad)
+            throw new InvalidOperationException($"Cannot load {weight}kg, max allowed: {MaxLoad - CurrentLoad}kg");
+        
+        CurrentLoad += weight;
+        Console.WriteLine($"Loaded {weight}kg into {SerialNumber}. Current load: {CurrentLoad}kg.");
+    }
+    
+    public void Unload()
+    {
+        CurrentLoad = 0;
+        Console.WriteLine($"Unloaded {SerialNumber}.");
+    }
+    
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Container {SerialNumber}:\n - Own Weight: {OwnWeight}kg\n - Max Load: {MaxLoad}kg\n - Height: {Height}cm\n - Depth: {Depth}cm\n - Cargo Weight: {CurrentLoad}kg");
+    }
 }
